666709e54743228a664fbc225abcd024a1ca899b
wrobbinz <wrobbinz@gmail.com>
2018-04-09 00:00:07 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+$(document)
+  .ready(() => {
+
+    $('.entry')
+      .transition({
+        animation: 'fade',
+        duration: '1.5s',
+      })
+
+    $('.lazyload')
+      .visibility({
+        type: 'image',
+        transition: 'fade in',
+        duration: 1000
+      })
+
+    // fix menu when passed
+    $('#fixedNavStart')
+      .visibility({
+        once: false,
+        onBottomPassed: () => {
+          $('.fixed.menu').transition('fade in')
+        },
+        onBottomPassedReverse: () => {
+          $('.fixed.menu').transition('fade out')
+        }
+      })
+
+    /* Change color of mobile menu icon when over white background */
+    $('#nav')
+      .visibility({
+        once: false,
+        onBottomPassed: () => {
+          $('#mobileNav i').css({
+            color: 'black',
+            opacity: 0.6
+          })
+        },
+        onBottomPassedReverse: () => {
+          $('#mobileNav i').css({
+            color: 'white',
+            opacity: 1
+          })
+        }
+      })
+
+    // create sidebar and attach to menu open
+    $('.ui.sidebar')
+      .sidebar('attach events', '#mobileNav')
+
+    // enable nav links
+    $('.nav-link').click(function() {
+      let href = $(this).attr('href')
+      if (href === '#offerings') {
+        href = '#fixedNavStart'
+      }
+      // on click, animate scroll
+      $('html, body').animate({
+        scrollTop: $(href).offset().top,
+      }, 500)
+    })
+  })
