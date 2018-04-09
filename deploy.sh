9be8e36e5acda469d7f4ce176ae08fea9e002819
wrobbinz <wrobbinz@gmail.com>
2018-04-09 00:28:57 -0700

    update deploy bugs

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 cd /var/www/calypharma.com
-git clone https://github.com/wrobbinz/calypharma.git html
+sudo git clone https://github.com/wrobbinz/calypharma.git html
 cd html
-npm install
+sudo npm install
 cd semantic
 gulp build
