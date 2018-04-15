79374ce3bb49d395fea92bde2e20f351a60b0d4c
Will Robbins <will@sighten.io>
2018-04-14 21:44:26 -0700

    update build process gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,57 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
 const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')
 const cleanCSS = require('gulp-clean-css')
 const autoprefixer = require('gulp-autoprefixer')
+const imagemin = require('gulp-imagemin')
 
 
-const jsDest = 'dist/js'
-
+/* HTML: Minify */
+gulp.task('html', () => {
+  gulp.src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'))
+})
+/* JavaScript: Transpile > Concat > Minify */
 gulp.task('scripts', () => {
   gulp.src('src/js/*.js')
     .pipe(babel()) // compile
-    .pipe(gulp.dest(jsDest))
     .pipe(concat('app.js')) // concat
-    .pipe(gulp.dest(jsDest))
     .pipe(rename('app.min.js'))
     .pipe(uglify()) // minify
-    .pipe(gulp.dest(jsDest))
+    .pipe(gulp.dest('dist/js'))
 })
 
+/* Sass/CSS: Compile (CSS) > Minify > AutoPrefix > Concat */
 gulp.task('styles', () => {
-  gulp.src('src/css/**/*.css')
+  gulp.src('src/sass/**/*.sass')
+    .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
     .pipe(concat('style.min.css'))
     .pipe(gulp.dest('dist/css'))
 })
+/* Images: Optimize */
+gulp.task('images', () => {
+  gulp.src('src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'))
+})
+/* Other */
+gulp.task('favicon', () => {
+  gulp.src('src/favicon.ico')
+    .pipe(gulp.dest('dist'))
+})
 
-gulp.task('build', ['scripts', 'styles'])
+gulp.task('other', ['favicon'])
 
+/* Build */
+gulp.task('build', ['html', 'scripts', 'styles', 'images', 'other'])
+/* Watch(build) */
 gulp.task('watch', () => {
   gulp.watch('src/**', ['build'])
 })
